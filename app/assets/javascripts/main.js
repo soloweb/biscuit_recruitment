99ff33c759cca9bd51c504f62c23d7955caa3e31
Jack Rehaag <jack@soloweb.com>
2014-09-18 09:54:34 +0100

    replaced css animation with jquery animation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,19 +2,11 @@
 
 $(document).ready(function () {
   $('.menu-toggle').click(function() {
-    $('.main-menu').addClass('animated fadeIn');
-    $('.main-menu').show();
-    setTimeout(function(){
-      $('.main-menu').removeClass('animated fadeIn');
-    }, 200);
+    $('.main-menu').fadeIn();
   });
 
   $('.main-menu .close').click(function() {
-    $('.main-menu').addClass('animated fadeOut');
-    setTimeout(function(){
-      $('.main-menu').removeClass('animated fadeOut');
-      $('.main-menu').hide();
-    }, 200);
+    $('.main-menu').fadeOut();
   });
   console.log('Design & Developed By Soloweb. 2014');
 })
