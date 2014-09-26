23411aeb826bb21a8df90ee85595b5b6d3436cf5
Jack Rehaag <jack@soloweb.com>
2014-09-26 15:56:45 +0100

    js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
-$(document).ready(function () {
-  $('.menu-toggle').click(function() {
-    $('.main-menu').stop().fadeIn();
-  });
+$('.menu-toggle').click(function() {
+  $('.main-menu').stop().fadeIn();
+});
 
-  $('.main-menu .close').click(function() {
-    $('.main-menu').stop().fadeOut();
-  });
+$('.main-menu .close').click(function() {
+  $('.main-menu').stop().fadeOut();
+});
+
+$(document).ready(function () {
   console.log('Design & Developed By Soloweb. 2014');
 })
